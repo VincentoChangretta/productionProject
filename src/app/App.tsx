f51635ef2c80e272e4d5b5130478b93937531108
Aram Papikyan <onlyphilosophylover@gmail.com>
2025-02-17 12:53:44 +0200

    git action

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,30 @@
-import React, { Suspense, useEffect } from 'react';
-import './styles/index.scss';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AppRouter } from 'app/providers/router';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
+import React, { Suspense, useEffect, useState } from "react";
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { Modal } from "shared/ui/Modal/Modal";
 
 function App() {
-    const { theme } = useTheme();
+  const { theme } = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <button onClick={() => setIsOpen(true)}>Toggle</button>
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}/>
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
         </div>
-    );
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,39 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { FC, ReactNode } from 'react';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  children?: ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
+}
+
+export const Modal: FC<ModalProps> = (props) => {
+    const {
+        children, className, isOpen, onClose,
+    } = props;
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+    };
+
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const closeHandler = () => {
+        if (onClose) {
+            onClose();
+        }
+    };
+
+    return (
+        <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={cls.overlay} onClick={closeHandler}>
+                <div className={cls.content} onClick={onContentClick}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
