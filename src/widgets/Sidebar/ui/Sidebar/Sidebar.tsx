bac1a099cba369a99c4ab8183707057c24859fdb
Aram Papikyan <onlyphilosophylover@gmail.com>
2025-02-17 14:35:39 +0200

    solved test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { t } from 'i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -23,12 +24,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 className,
             ])}
         >
-            {/* <Button
-                data-testid="sidebar-toggle"
-                onClick={onToggle}
-            >
-                toggle
-            </Button> */}
+            <Button data-testid="sidebar-toggle" onClick={onToggle}>
+                {t('toggle')}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
