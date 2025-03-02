6dbb372f094696f43049456d6687f6ded88c1ead
Aram Papikyan <onlyphilosophylover@gmail.com>
2025-03-02 18:21:22 +0200

    fix problems

--- src/enteties/Counter/model/selectors/getCounter/getCounter.test.ts
+++ src/enteties/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -1,12 +1,12 @@
+import { DeepPartial } from "@reduxjs/toolkit";
 import { StateSchema } from "app/providers/StoreProvider";
 import { getCounter } from "./getCounter";
-import { DeepPartial } from "@reduxjs/toolkit";
 
 describe('getCounter', () => {
     test('state', () => {
         const state: DeepPartial<StateSchema> = {
-            counter: {value: 10}
-        }
-        expect(getCounter(state as StateSchema)).toEqual({value: 10})
+            counter: { value: 10 },
+        };
+        expect(getCounter(state as StateSchema)).toEqual({ value: 10 });
     });
 });
